fd26edbf7d5900fe8142c8a8bb306d7c0fd352c4
Georgi <georgi.kostov@haemimont.com>
2020-01-14 14:23:27 +0200

    initial

---
+++ src/main/java/com/ds/mdb/dsmdb/DSMDBApplication.java
@@ -0,0 +1,13 @@
+package com.ds.mdb.dsmdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DSMDBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DSMDBApplication.class, args);
+	}
+
+}
